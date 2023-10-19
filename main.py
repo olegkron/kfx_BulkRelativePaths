20e05f4a77ead26fec9e6f572a8744a9371d13dd
olegkron <contact@olegkron.cc>
2023-10-19 10:39:43 +0100

    main.py

--- main.py
+++ main.py
@@ -1,31 +1,22 @@
-# Bulk relative paths for SHOTS folder
-# 1. scan the SHOTS dir for shots and find the latest version of the script
-# 2. makes a backup of the script to the same location
-# 3. replaces all file paths in the script with relative paths as well as sets the project directory to \[python \{nuke.script_directory()\}]
-# 4. saves the script
-# logs which scripts were changed
 import argparse
 
 from bulk_process_shots import bulk_process_shots
 from bulk_restore_backups import bulk_restore_backups
 
-# print(f"Updated project directory in: {latest_script}")
-
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Bulk processing of shots or restoring backups')
-	parser.add_argument('--restore', '-r', action='store_true', help='Restore backups instead of processing shots')
-	parser.add_argument('--shots_dir_path', '-s', help='Path to SHOTS directory')
-	parser.add_argument('--move_assets', '-a', action='store_true', help='Move assets to PLATES directory')
+	parser.add_argument('-restore', '-r', action='store_true', help='Restore backups instead of processing shots')
+	parser.add_argument('-shots', '-s', help='Path to SHOTS directory')
+	parser.add_argument('-move_assets', '-a', action='store_true', help='Move assets to PLATES directory')
 	args = parser.parse_args()
 
-	shots_dir_path = "/Users/koalamac/Desktop/SHOTS"
 	should_move_assets = False
 
-	if args.shots_dir_path: shots_dir_path = args.shots_dir_path
-	if args.move_assets: should_move_assets = True
-	# shots_dir_path = "/Users/koalamac/Desktop/SHOTS"
-
-	if args.restore:
-		bulk_restore_backups(shots_dir_path)
+	if args.shots:
+		shots_dir_path = args.shots
+		if args.restore:
+			bulk_restore_backups(shots_dir_path)
+		else:
+			bulk_process_shots(shots_dir_path, should_move_assets)
 	else:
-		bulk_process_shots(shots_dir_path, should_move_assets)
+		print("Please provide a path to the SHOTS directory with -shots=PATH_TO_SHOTS")
